8d74ed48c06f8b6a17540658df2aa9b1abeec07a
Catriflac <115920743+Catriflac@users.noreply.github.com>
2022-10-18 17:05:41 +0200

    Update FFXP8S_beadando_2.c

--- FFXP8S_beadando_2.c
+++ FFXP8S_beadando_2.c
@@ -1,55 +1,73 @@
 #include <stdio.h>
+#include <math.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
 //gcc -W -Wall -Wextra -pedantic FFXP8S_beadando_2.c -o cprogram
 //.\cprogram
 
-char jegyekRaw[] = "Jegyek input stringje";
-char sulyokRaw[] = "Sulyok input stringje";
+char jegyekRaw[300] = "Jegyek input stringje";
+char sulyokRaw[300] = "Sulyok input stringje";
 
-int jegyek[300];
-int sulyok[300];
+int jegyek[100];
+int sulyok[100];
 
 int i = 0;
 char seps[]   = " ,\t\n";
 char *token;
 int tokenInt;
 
+int isNumber(char token[]);
+
 int main( void )
 {
     printf("Adja meg a jegyeket: ");
     fgets(jegyekRaw, sizeof(jegyekRaw), stdin);  // read jegyekRaw
 
-    printf( "A megadott jegyek:\n" );
     token = strtok( jegyekRaw, seps );
-    
+
     while( token != NULL )
     {
         sscanf(token, "%d", &tokenInt);
-        if(tokenInt > 0 && tokenInt <6)
+        if( isNumber(token)  && tokenInt < 6 && tokenInt > 0 )
         {
-            printf( " %d\n", tokenInt );
+            //printf( "%d ", tokenInt );
             jegyek[i] = tokenInt;
             token = strtok( NULL, seps );
             i++;
         }
         else{
             printf( "Hibas bemenet mellozese! (%s)\n", token );
+            token = strtok( NULL, seps );
+            i++;
         }
     }
-    printf("%d\n", jegyek[3]);
 
-
-    printf("Adja meg a jegyekhez tartozo sulyokat: ");
-    fgets(sulyokRaw, sizeof(sulyokRaw), stdin);  // read sulyokRaw
-
-    printf( "A megadott sulyok:\n" );
-    token = strtok( sulyokRaw, seps );
-    while( token != NULL )
-    {
-        printf( " %s ", token );
-        token = strtok( NULL, seps );
+    printf( "A megadott jegyek:\n" );    
+    for (int j = 0; j < i; j++){
+        if(jegyek[j] != 0)
+        {
+            printf("%d ", jegyek[j]);   
+        }  
     }
+    printf("\n");
 
+
+}
+
+int isNumber(char token[])
+{
+    for (int i = 0; token[i]!= '\0'; i++)
+    {
+        if(strstr(token, "."))
+            {
+            printf("Valos bemenet! Tort resz eldobasa!\n");
+            return token[i];
+            }
+            
+        if (isdigit(token[i]) == 0)
+            return 0;
+    }
+    return 1;
 }
